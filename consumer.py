0c234546ae93f1515e58925df5039e64383a3d91
SarahEmaAllam <s.allam@student.rug.nl>
2024-02-20 20:23:23 +0100

    metadata problem in the topic of the producer, but otherwise the components run:

--- consumer.py
+++ consumer.py
@@ -3,13 +3,14 @@ from kafka import KafkaConsumer
 import json
 
 consumer = KafkaConsumer(
-    'randomNumbers',
-     bootstrap_servers=['kafka:9092', '127.0.0.1:9092'],
+    'data_stream',
+    bootstrap_servers=['localhost:9092'],
+    api_version=(0, 11, 5),
      auto_offset_reset='earliest',
      enable_auto_commit=True,
      group_id='my-group',
      value_deserializer=lambda x: json.loads(x.decode('utf-8')))
 
 for message in consumer:
-    message = message.value
+    # message = message.value
     print(f"Received data: {message}")
--- producer.py
+++ producer.py
@@ -4,12 +4,14 @@ import time
 import json
 import random
 
-producer = KafkaProducer(bootstrap_servers=['kafka:9092', '127.0.0.1:9092'],
+producer = KafkaProducer(
+    bootstrap_servers=['localhost:9092'],
+                        api_version=(0, 11, 5),
                          value_serializer=lambda x:
                          json.dumps(x).encode('utf-8'))
 
 while True:
     data = {'number': random.random()}
-    producer.send('randomNumbers', value=data)
+    producer.send('data_stream', data)
     print(f"Sent data: {data}")
     time.sleep(1)
