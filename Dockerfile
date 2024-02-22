f00d3fac35585993bb07261c65d3abd3063b4742
SarahEmaAllam <s.allam@student.rug.nl>
2024-02-22 11:37:28 +0100

    need to install confluent kafka in docker compose but that is really hard to do with this linux distribution

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,12 @@
 FROM python:3.8-slim
 
-RUN pip install kafka-python
-
+# Install dependencies and utilities
+RUN apt-get update && \
+    apt-get install -y netcat-openbsd
+# Copy the Python script and other necessary files
 COPY . /app
 
 WORKDIR /app
+
+# Wait for Kafka to be ready before creating the topic
+CMD ["sh", "-c", "while ! nc -z broker 9092; do sleep 1; done && confluent local kafka topic create data-stream"]
--- producer.py
+++ producer.py
@@ -1,17 +1,46 @@
-# producer.py
-from kafka import KafkaProducer
-import time
-import json
+#!/usr/bin/env python
 import random
+import sys
+from random import choice
+from argparse import ArgumentParser, FileType
+from configparser import ConfigParser
+from confluent_kafka import Producer
 
-producer = KafkaProducer(
-    bootstrap_servers=['localhost:9092'],
-                        api_version=(0, 11, 5),
-                         value_serializer=lambda x:
-                         json.dumps(x).encode('utf-8'))
+if __name__ == '__main__':
+    # Parse the command line.
+    parser = ArgumentParser()
+    parser.add_argument('config_file', type=FileType('r'))
+    args = parser.parse_args()
 
-while True:
-    data = {'number': random.random()}
-    producer.send('data_stream', data)
-    print(f"Sent data: {data}")
-    time.sleep(1)
+    # Parse the configuration.
+    # See https://github.com/edenhill/librdkafka/blob/master/CONFIGURATION.md
+    config_parser = ConfigParser()
+    config_parser.read_file(args.config_file)
+    config = dict(config_parser['default'])
+
+    # Create Producer instance
+    producer = Producer(config)
+
+    # Optional per-message delivery callback (triggered by poll() or flush())
+    # when a message has been successfully delivered or permanently
+    # failed delivery (after retries).
+    def delivery_callback(err, msg):
+        if err:
+            print('ERROR: Message failed delivery: {}'.format(err))
+        else:
+            print("Produced event to topic {topic}: key = {key:12} value = {value:12}".format(
+                topic=msg.topic(), key=msg.key().decode('utf-8'), value=msg.value().decode('utf-8')))
+
+    # Produce data by selecting random values from these lists.
+    topic = "data_stream"
+    # user_ids = ['eabara', 'jsmith', 'sgarcia', 'jbernard', 'htanaka', 'awalther']
+    # products = ['book', 'alarm clock', 't-shirts', 'gift card', 'batteries']
+
+    while True:
+        data = {random.random()}
+        key = 'number'
+        producer.produce(topic, data, key, callback=delivery_callback)
+
+        # Block until the messages are sent.
+        producer.poll(10000)
+        producer.flush()
