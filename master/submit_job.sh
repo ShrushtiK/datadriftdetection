91c3161d58656929df4950fa848cc54dc8eefc68
Shrushti Kaul <shrushtikaul05@gmail.com>
2024-03-06 07:18:02 +0000

    Train and test gbt on dataset

--- master/submit_job.sh
+++ master/submit_job.sh
@@ -6,4 +6,4 @@
 #done
 
 # Submit the Spark job
-/opt/bitnami/spark/bin/spark-submit --master spark://spark-master:7077 --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.4.1,com.datastax.spark:spark-cassandra-connector_2.12:3.4.1,saurfang:spark-sas7bdat:3.0.0-s_2.12 /usr/app/gbt_model.py
+/opt/bitnami/spark/bin/spark-submit --master spark://spark-master:7077 --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.4.1,com.datastax.spark:spark-cassandra-connector_2.12:3.4.1,saurfang:spark-sas7bdat:3.0.0-s_2.12 /usr/app/spark_stream.py
